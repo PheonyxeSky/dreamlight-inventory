0db3d61bf161b909e2b0817d31727c5ce33c61e8
Jennifer <jkbleam@gmail.com>
2026-03-05 15:35:18 -0600

    Removing all old Co-Pilot files - starting fresh

--- convert-ingredients.js
+++
@@ -1,9 +0,0 @@
-const csv = require("csvtojson");
-const fs = require("fs");
-
-csv()
-  .fromFile("./data/Ingredients.csv")
-  .then((jsonObj) => {
-    fs.writeFileSync("./public/data/ingredients.json", JSON.stringify(jsonObj, null, 2));
-    console.log("Ingredients.json created!");
-  });
